eb4499ac67ae99f91721a379e89edd294d7375d1
Utkarsh Kushwaha <175150344+cyberdefenc@users.noreply.github.com>
2025-06-18 11:33:52 +0530

    Delete Python directory

--- Python/Codings/P3_geometry_.py
+++
@@ -1,5 +0,0 @@
-from tkinter import *
-
-py_windows = Tk()
-py_windows.geometry("733x434")
-py_windows.mainloop()
--- Python/Codings/P4_title_.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-root = Tk()
-root.geometry("444x233")
-root.title("Shadow GUI")
-
-# Import
-
-root.mainloop()
